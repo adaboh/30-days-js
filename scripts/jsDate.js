e6de365f7671c2840e0155e2753cf543bc16d6bd
adaboh <kweada@gmail.com>
2021-02-16 21:32:42 -0500

    added digital clock to day 2

---
+++ scripts/jsDate.js
@@ -0,0 +1,46 @@
+// console.log("Welcome to JS Date AND TIME")
+
+// const time = new Date()
+// let epochTime = new Date(0)
+// console.log(epochTime)
+
+/* function Person(person_firstName, person_age, person_homeTown) {
+  this.firstName = person_firstName;
+  this.age = person_age;
+  this.homeTown = person_homeTown;
+}
+
+let persons = [];
+const person1 = new Person("Reeves", 32, "Columbus ");
+// Person.prototype.nationality = "Ghanaian";
+
+function createPerson(firstname, age, hometown) {
+  let person = new Person(firstname, age, hometown);
+
+  persons.push(person);
+}
+
+createPerson("Punit", 23, "Tardi");
+createPerson("One way", 33, "Direction");
+ */
+
+/*  function Person(first, last, age, eyecolor) {
+   this.firstName = first;
+   this.lastName = last;
+   this.age = age;
+   this.eyeColor = eyecolor;
+ }
+
+ Person.prototype.name = function () {
+   return this.firstName + " " + this.lastName;
+ };
+
+ let kojo = new Person("kojo", "Amoah", 14, "black") */
+
+
+ const newDate = new Date()
+//  console.log(newDate.getMilliseconds())
+
+ const birthDay = new Date(1992, 5, 10)
+
+ console.log(birthDay.getSeconds())
