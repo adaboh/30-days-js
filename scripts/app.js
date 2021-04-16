ac2564016ace755d73c0e69194b5360bb368417a
adaboh <kweada@gmail.com>
2021-04-15 20:33:48 -0400

    completed day3

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,27 @@
-//!
-setInterval(setClock, 1000);
+//! day 3 input
+const inputs = document.querySelectorAll(".container input");
 
-const hourHand = document.querySelector("[data-hour]");
-const minsHand = document.querySelector("[data-mins]");
-const secsHand = document.querySelector("[data-secs]");
-
-function setClock() {
-  function removeTransition(handRatio, ele) {
-    if (handRatio === 0) {
-      ele.classList.add("no-transition");
-    } else {
-      ele.classList.remove("no-transition");
-    }
-  }
-  const currentDate = new Date();
-
-  const secsRatio = currentDate.getSeconds() / 60;
-  const minsRatio = (secsRatio + currentDate.getMinutes()) / 60;
-  const hoursRatio = (minsRatio + currentDate.getHours()) / 12;
-  setRotation(secsHand, secsRatio);
-  setRotation(minsHand, minsRatio);
-  setRotation(hourHand, hoursRatio);
-  removeTransition(secsRatio, secsHand);
-  removeTransition(minsRatio, minsHand);
-  removeTransition(hoursRatio, hourHand);
+function handleInput() {
+  const suffix = this.dataset.sizing || "";
+  const hightLight = document.querySelector(".h1");
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  hightLight.style.color = `--${this.value}`
+  // console.log(this.dataset)
 }
 
-function setRotation(ele, ratio) {
-  ele.style.setProperty("--hand-rotation", ratio * 360);
-}
-setClock();
+/* 
+ele.style.setProperty(pro, value), this automatically appends : to the property value and hence no need for ":"
+*/
+
+inputs.forEach((input) => {
+  input.addEventListener("change", handleInput);
+});
+
+
+
+const inputColor = document.querySelector(".wrap-color");
+inputColor.addEventListener("change", function(){
+  const wrap  = document.querySelector(".wrap")
+  wrap.style.setProperty(`--${this.name}`, this.value)
+ 
+})
