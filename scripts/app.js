83dda0adc4784be7c727936de685ed7eaa0d958b
adaboh <kweada@gmail.com>
2021-04-23 16:08:56 -0400

    fix transitionend bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,22 @@
-const panels = document.querySelectorAll(".panel")
+const panels = document.querySelectorAll(".panel");
 
-function toggleOpen(){
-  this.classList.toggle("open")
-  
+function toggleOpen() {
+  this.classList.toggle("open");
 }
 
-function toggleActice(e){
-  this.classList.toggle("open-active")
+function toggleActice(e) {
+  // console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
 }
 
-panels.forEach((panel)=> panel.addEventListener("click", toggleOpen))
-panels.forEach((panel)=> panel.addEventListener("click", toggleActice))
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) => panel.addEventListener("transitionend", toggleActice));
 
 const screenWidth = window.innerWidth;
-console.log("screenWidth", screenWidth)
 
-function resizeWindow(){
+function resizeWindow() {
   if (screenWidth < 768) {
     const msg = "💡 ➡ To have a better experience on this page, position your device horizontally. 😉";
     alert(msg);
@@ -27,7 +28,7 @@ function resizeWindow(){
 //   alert(msg)
 // }
 
-window.addEventListener("load",resizeWindow)
+window.addEventListener("load", resizeWindow);
 // window.addEventListener("resize", resizeWindow)
 
 // console.log(window)
