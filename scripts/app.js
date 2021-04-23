d435f499076084ec9fcab564dd65c51988964677
adaboh <kweada@gmail.com>
2021-04-23 16:03:03 -0400

    day6 starter files

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,4 @@
-const panels = document.querySelectorAll(".panel")
+/* 
+Type Ahead: AJAX
 
-function toggleOpen(){
-  this.classList.toggle("open")
-  
-}
-
-function toggleActice(e){
-  this.classList.toggle("open-active")
-}
-
-panels.forEach((panel)=> panel.addEventListener("click", toggleOpen))
-panels.forEach((panel)=> panel.addEventListener("click", toggleActice))
-
-const screenWidth = window.innerWidth;
-console.log("screenWidth", screenWidth)
-
-function resizeWindow(){
-  if (screenWidth < 768) {
-    const msg = "💡 ➡ To have a better experience on this page, position your device horizontally. 😉";
-    alert(msg);
-  }
-}
-
-// if(screenWidth < 768){
-//   const msg = "💡 ➡ To have a better experience on this page, position your device horizontally. 😉";
-//   alert(msg)
-// }
-
-window.addEventListener("load",resizeWindow)
-// window.addEventListener("resize", resizeWindow)
-
-// console.log(window)
+*/
